9b4bf1324fb00092e43d7c832e9d91cd03d7c118
Ethan <ethan.tran@vinova.com.sg>
2025-04-19 01:24:27 +0700

    [BE][FEATURE]: thanh vjp add chat feature
    [][BE][FIXED]: thanh vjp add feature to fix cors origin
    
    [BE][FEATURE]: thanh vjp add chat feature
    
    [BE][FIXED]: thanh vjp add feature to fix cors origin

---
+++ Project.API/SignalR/Hubs/MessagingHub.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.SignalR;
+using Project.API.SignalR.Service;
+using Project.Core.Entities.Business.DTOs.MessageDTOs;
+using Project.Core.Entities.General;
+using Project.Core.Interfaces.IRepositories;
+
+namespace Project.API.SignalR.Hubs {
+    public class MessagingHub : Hub {
+        private readonly IMessageService _messageService;
+
+        private static readonly List<UserMessage> MessageHistory = new List<UserMessage>();
+
+        public MessagingHub(IMessageService messageService) {
+            _messageService = messageService;
+        }
+
+        public async Task PostMessage(string content, string senderName, string receiverName) {
+            var userMessage = new MessageCreateRequest {
+                Content = content,
+                ReceiverUserName = receiverName,
+                SenderUserName = senderName,
+                DateTime = DateTime.UtcNow
+            };
+
+            var message = await _messageService.SendMessage(userMessage);
+
+            await Clients.Others.SendAsync("ReceiveMessage" + receiverName, message.MessageId, senderName, content, userMessage.DateTime);
+        }
+
+        public async Task RetrieveMessageHistory() =>
+            await Clients.Caller.SendAsync("MessageHistory", MessageHistory);
+    }
+}
